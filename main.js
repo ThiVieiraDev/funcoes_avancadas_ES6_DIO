ac5a9e97b797a32f5ae3e203aaa31938e83e24ab
Thi Vieira <thiiagoviieira@gmail.com>
2021-01-05 00:04:12 -0300

    Rest e Spread Operator

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+// Destructuring Assignment
+var arr = [{ name: 'Apple', type: 'friot' }];
+
+var [{ name: fruitName }] = arr;
+console.log(name);
+var [{ name }] = arr;
+console.log(name);
+
+
+
+
+
+
+/* 
+Rest e Spread Operator
+
+*Rest* permite que uma função receba um número indefinido de parâmetros e spread permite definir um número indefinido de parâmetros. Os operadores rest e spread permitem trabalhar com múltiplos parâmetro em funções.
+*/
