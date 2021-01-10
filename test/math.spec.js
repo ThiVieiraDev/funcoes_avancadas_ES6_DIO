26b74dbc54040ba5bf707d6acbcede95a7bdda6c
Thi Vieira <thiiagoviieira@gmail.com>
2021-01-10 03:23:44 -0300

    Testando diversos testes com it, it.skip e it.only. Testando também com hooks beforeEach e afterEach

--- test/math.spec.js
+++ test/math.spec.js
@@ -1,13 +1,31 @@
 const assert = require('assert');
 const Math = require('./math.js');
 
+let value = 5;
+
 describe('Math class', function () {
+    //hooks
+    // existe o beforeEach e o afterEach
+    beforeEach(function() {
+        value = 0;
+    });
+
+
     it('Sum two numbers', function (done) {
         const math = new Math();
+        this.timeout(3000);
 
-        math.sum(5, 5, value => {
+        value = 5;
+
+        math.sum(value, 5, value => {
             assert.equal(value, 10);
             done();
         });
+    });
+
+    it('Multiply two numbers', function (){
+        const math = new Math();
+
+        assert.equal(math.multiply(value,5), 0);
     });
 });
