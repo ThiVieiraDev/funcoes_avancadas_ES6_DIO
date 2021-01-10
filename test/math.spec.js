21e41be5e1c8ad03d55e563504f1a46fc8864ecf
Thi Vieira <thiiagoviieira@gmail.com>
2021-01-10 03:36:12 -0300

    Inicializando o sinon: npm i --dev-save sinon

--- test/math.spec.js
+++ test/math.spec.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const Math = require('./math.js');
+const expect = require('chai').expect;
 
 let value = 5;
 
@@ -18,14 +19,25 @@ describe('Math class', function () {
         value = 5;
 
         math.sum(value, 5, value => {
-            assert.equal(value, 10);
+            expect(value).to.equal(10);
             done();
         });
     });
 
-    it('Multiply two numbers', function (){
+    it.only('Multiply two numbers', function (){
         const math = new Math();
+        const obj = {
+            name: 'Thiago Vieira'
+        };
+        const obj2 = {
+            name: 'Thiago Vieira'
+        };
+        // assert.equal(math.multiply(value,5), 0);
+        // expect(math.multiply(value, 5)).to.equal(0);
+        // expect(obj)
+        // .to.have.property('name')
+        // .equal('Thiago Vieira');
+        expect(obj).to.deep.equal(obj2);
 
-        assert.equal(math.multiply(value,5), 0);
     });
 });
